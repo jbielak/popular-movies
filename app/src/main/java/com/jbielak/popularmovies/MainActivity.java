ec7c57dc56203b7b2e6ab2e358bb0d53ee42bd05
jbielak <justyna_bielak@outlook.com>
2018-02-25 23:13:21 +0100

    Add app menu

--- app/src/main/java/com/jbielak/popularmovies/MainActivity.java
+++ app/src/main/java/com/jbielak/popularmovies/MainActivity.java
@@ -2,6 +2,8 @@ package com.jbielak.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mian, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int clickedItemId = item.getItemId();
+
+        if (clickedItemId == R.id.action_sort_by_most_popular) {
+            //TODO: Implement sorting films by most popular
+        }
+        if (clickedItemId == R.id.action_sort_by_most_highest_rated) {
+            //TODO: Implement sorting films by highest rated
+        }
+        return false;
     }
 }
