bf198e9e69b52ad97f76750f577305f8ebf98993
jbielak <justyna_bielak@outlook.com>
2018-03-01 18:06:34 +0100

    feat: Display posters in grid

--- app/src/main/java/com/jbielak/popularmovies/MainActivity.java
+++ app/src/main/java/com/jbielak/popularmovies/MainActivity.java
@@ -2,15 +2,57 @@ package com.jbielak.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.jbielak.popularmovies.adapter.MovieAdapter;
+import com.jbielak.popularmovies.model.Movie;
+import com.jbielak.popularmovies.utilities.MovieDeserializer;
+import com.jbielak.popularmovies.utilities.NetworkUtils;
+import com.jbielak.popularmovies.utilities.PathProvider;
+import com.jbielak.popularmovies.utilities.ResponseListener;
+import com.jbielak.popularmovies.utilities.SortType;
+
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.List;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUMBER_OF_COLUMNS = 2;
+
+    private RecyclerView mMoviesRecyclerView;
+    private MovieAdapter mMovieAdapter;
+    private GridLayoutManager gridLayoutManager;
+
+    private List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        NetworkUtils.getInstance(getApplicationContext());
+
+        if (NetworkUtils.isOnline(getApplicationContext())) {
+            getMovies(SortType.RATING);
+        } else {
+            Toast.makeText(this, getString(R.string.no_internet_connection), Toast.LENGTH_SHORT)
+                    .show();
+        }
+
+        mMoviesRecyclerView = findViewById(R.id.recyclerView);
+        mMoviesRecyclerView.setHasFixedSize(true);
+        gridLayoutManager = new GridLayoutManager(this, NUMBER_OF_COLUMNS);
+        mMoviesRecyclerView.setLayoutManager(gridLayoutManager);
     }
 
     @Override
@@ -30,5 +72,24 @@ public class MainActivity extends AppCompatActivity {
             //TODO: Implement sorting films by highest rated
         }
         return false;
+    }
+
+    private void getMovies(SortType sortMethod) {
+        URL url = PathProvider.getMoviesUrl(sortMethod);
+        NetworkUtils.getInstance()
+                .get(url, new ResponseListener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        movies = MovieDeserializer.deserializeMovies(response);
+                        mMovieAdapter = new MovieAdapter(MainActivity.this, movies);
+                        mMoviesRecyclerView.setAdapter(mMovieAdapter);
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.error_fetch_movies_data), Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/jbielak/popularmovies/adapter/MovieAdapter.java
@@ -0,0 +1,60 @@
+package com.jbielak.popularmovies.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.jbielak.popularmovies.R;
+import com.jbielak.popularmovies.model.Movie;
+import com.jbielak.popularmovies.utilities.PathProvider;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Justyna on 2018-02-28.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private List<Movie> movies;
+    private Context context;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        Picasso.with(context)
+                .load(PathProvider.getPosterUrl(movie.getPosterPath()).toString())
+                .into(holder.posterImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView posterImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            posterImageView = itemView.findViewById(R.id.image_view_poster);
+        }
+
+    }
+}
