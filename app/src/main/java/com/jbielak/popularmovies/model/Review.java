91c21080a42fe2961a03ee764765d2fad2937e8b
jbielak <justyna_bielak@outlook.com>
2018-03-13 16:32:08 +0100

    feat: Add Review and ReviewResponse models

---
+++ app/src/main/java/com/jbielak/popularmovies/model/Review.java
@@ -0,0 +1,84 @@
+package com.jbielak.popularmovies.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Justyna on 2018-03-12.
+ */
+
+public class Review implements Parcelable{
+
+    private String id;
+    private String author;
+    private String content;
+    private String url;
+
+    public Review() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.id);
+        parcel.writeString(this.author);
+        parcel.writeString(this.content);
+        parcel.writeString(this.url);
+    }
+
+    private Review(Parcel in) {
+        id = in.readString();
+        author = in.readString();
+        content = in.readString();
+        url = in.readString();
+    }
+
+    public static final Parcelable.Creator<Review> CREATOR = new Parcelable.Creator<Review>() {
+
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/jbielak/popularmovies/model/ReviewResponse.java
@@ -0,0 +1,56 @@
+package com.jbielak.popularmovies.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Justyna on 2018-03-12.
+ */
+
+public class ReviewResponse {
+
+    @SerializedName("id")
+    private long id;
+
+    @SerializedName("page")
+    private int page;
+
+    @SerializedName("total_pages")
+    private int totalPages;
+
+    @SerializedName("results")
+    private List<Review> results;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setResults(List<Review> results) {
+        this.results = results;
+    }
+
+    public List<Review> getResults() {
+        return results;
+    }
+}
